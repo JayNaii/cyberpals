5424837402e7e732ef982fcfc93d29c598251b6e
JayNaii <destroleague@gmail.com>
2023-05-18 13:56:37 -0300

    Updates

---
+++ precache-manifest.b93ce9f6d65b660117f05c82257813ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d63e8e19e1d9f52620",
+    "url": "/cyberpals/static/css/main.7416c1ae.chunk.css"
+  },
+  {
+    "revision": "83d63e8e19e1d9f52620",
+    "url": "/cyberpals/static/js/main.83d63e8e.chunk.js"
+  },
+  {
+    "revision": "b9bd24d99d16fd825b22",
+    "url": "/cyberpals/static/css/1.feaa70bf.chunk.css"
+  },
+  {
+    "revision": "b9bd24d99d16fd825b22",
+    "url": "/cyberpals/static/js/1.b9bd24d9.chunk.js"
+  },
+  {
+    "revision": "8e31e3eb963fb298b143",
+    "url": "/cyberpals/static/js/runtime~main.8e31e3eb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cyberpals/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c59158ef8cdbdad42f0ba54b376e9f43",
+    "url": "/cyberpals/index.html"
+  }
+];
